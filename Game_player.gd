8ea24d1e7bc5cac74076c70d4429a12a249e7173
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-01-17 09:56:00 +1300

    2.5 hr 2nd area finished going on to third
    BTW have not done any drawings so there is literally no decorations or sprites. Besides from that where I worked up to is finished as just needs the sprites to be placed in

--- Game_player.gd
+++ Game_player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 var vert = false
+var kurt = false
 var four = false
 var twojump = true
 var e = false
+var portal = false
+var vtv = false
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -33,6 +36,15 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	if vert == true:
 		$"../../Camera/Camera2D".position.y = $".".position.y
+	if kurt == true:
+		$"../../Camera/Camera2D".position.x = $".".position.x
+	if portal == true:
+		if Input.is_action_just_pressed("ui_e"):
+			vtv = true
+			four = false
+			$"../../HtoS/Portal/AnimatedSprite2D".play("Portal")
+	if vtv == true:
+		$"../../HtoS/Portal/Path2D/PathFollow2D".progress += 10
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -60,3 +72,25 @@ func _on_homeward_body_entered(body: Node2D) -> void:
 		twojump = true
 		$"../../Camera/Camera2D".position = Vector2(0,0)
 		$".".position = Vector2(500,0)
+
+
+func _on_portal_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		portal = true
+
+
+func _on_portal_body_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		portal = false
+
+
+func _on_portalmove_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		vtv = false
+		$"../../HtoS/Portal/Path2D/PathFollow2D".progress = 0
+		vert = false
+		kurt = true
+		four = false
+		twojump = true
+		$".".position = Vector2(2000,-4000)
+		$"../../Camera/Camera2D".position = $".".position
