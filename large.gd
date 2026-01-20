c5bd88d4cb5dec0e7464d5a4ea5eee2c39cccc14
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-01-20 14:40:20 +1300

    7hr 30min on dot, Large room mechanics still being edited
    BTW time I write down is hackatime approx for all the times

--- large.gd
+++ large.gd
@@ -2,6 +2,9 @@ extends Node2D
 var chickensthere = 0
 var Hidden = false
 var pillarthere = 0
+var tekau = false
+var toru = false
+var v = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,6 +21,7 @@ func _process(delta: float) -> void:
 			$Chicken/Chicken/Chicken.play("Almost")
 		else:
 			$Chicken/Chicken/Chicken.play("Done")
+			toru = true
 	if pillarthere > -1:
 		if pillarthere == 0:
 			$Chicken/Chicken2/Chicken.play("First")
@@ -27,7 +31,24 @@ func _process(delta: float) -> void:
 			$Chicken/Chicken2/Chicken.play("Almost")
 		else:
 			$Chicken/Chicken2/Chicken.play("Done")
-	
+			tekau = true
+	if v == false:
+		$TnT/TnT.play("No")
+		$TnT/TnT2.play("No")
+		$TnT/TnT3.play("No")
+		$TnT/TnT4.play("No")
+		$TnT/TnT5.play("No")
+		$TnT/TnT6.play("No")
+	if tekau == true:
+		if toru == true:
+			if v == true:
+				$TnT/TnT.play("Yes")
+				$TnT/TnT2.play("Yes")
+				$TnT/TnT3.play("Yes")
+				$TnT/TnT4.play("Yes")
+				$TnT/TnT5.play("Yes")
+				$TnT/TnT6.play("Yes")
+
 
 
 func _on_chicken_area_entered(area: Area2D) -> void:
@@ -41,9 +62,24 @@ func _on_hiddenfire_area_entered(area: Area2D) -> void:
 		$"Small flame/Hiddenfire/HideFlame".play("On")
 		Hidden = true
 		print("Hidden")
-		
+		$TnT/Lever/lol.play("Up")
 
 func _on_chicken_2_area_entered(area: Area2D) -> void:
 	if area.name == "Fireball":
 		pillarthere = pillarthere + 1
 		
+
+
+func _on_lever_area_entered(area: Area2D) -> void:
+	if area.name == "Fireball":
+		if Hidden == true:
+			if toru == true:
+				if tekau == true:
+					$Door/Pile/PathFollow2D.progress_ratio = 0.9
+					$TnT/Lever/lol.play("Down")
+					v = false
+					$Door/Explode.play("Explo")
+
+
+func _on_leave_large_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
